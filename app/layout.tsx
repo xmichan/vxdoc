a8a069d03aec9da4c84ffea88f888988a4f65f21
vx <v4174675@gmail.com>
2026-06-07 14:11:15 +0800

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import './global.css';
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8299409632430664"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="flex min-h-screen flex-col">{children}</body>
     </html>
   );
